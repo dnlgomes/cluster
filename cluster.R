096ba0d40034845b0da91f6bbf4c1e5fc193a4d5
Danilo Gomes <dnlgomes93@gmail.com>
2016-02-21 01:26:31 -0300

    starting clustering

--- cluster.R
+++ cluster.R
@@ -7,3 +7,13 @@ movies$ReleaseDate = NULL
 movies$VideoReleaseDate = NULL
 movies$IMDB = NULL
 movies = unique(movies)
+
+
+####### Hierarquical Cluster ################
+distances = dist(movies[2:20], method = "euclidean")
+clusterMovies = hclust(distances, method = "ward.D")
+plot(clusterMovies)
+clusterGroups = cutree(clusterMovies, k = 10)
+subset(movies, Title=="Men in Black (1997)")
+clusterGroups[257]
+cluster2 = subset(movies, clusterGroups == 2)
